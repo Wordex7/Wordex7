0f5d9521e2b87589a77c9d8a394ae01cee4e6b31
Wordex7 <136884108+Wordex7@users.noreply.github.com>
2023-06-17 11:26:45 +0300

    Update Yapılandırma.js

--- Yapılandırma.js
+++ Yapılandırma.js
@@ -2,4 +2,9 @@ module.exports =  {
    token: "4018f45004a6bcd711eaa4c0afe3fbfe6c9a3edd041b88fc52ff5d92972eed51"
    prefix: "!"
    footer: "Tc İllegallik Team",
-   guildİD: "984404289608908860"
+   guildİD: "984404289608908860",
+   başvuruYt: "950382314897150003",
+   yetkirolleri: "984406479702482954",
+   logKanalı: "1119542022584418334",
+   başvuruDurum: "1119542292529807481",
+   
