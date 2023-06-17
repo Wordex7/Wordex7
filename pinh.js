5a39acb6b59944236dda662d47c0fac597dd89a9
Wordex7 <136884108+Wordex7@users.noreply.github.com>
2023-06-17 10:30:03 +0300

    Create pinh.js

---
+++ pinh.js
@@ -0,0 +1,21 @@
+const { EmbedBuilder } = require("discord.js");
+
+exports.run = async (client, message, args) => {
+
+  
+
+  return message.reply(`${client.ws.ping}ms`)
+
+};
+
+exports.conf = {
+
+  aliases: ["pong"]
+
+};
+
+exports.help = {
+
+  name: "ping"
+
+};
